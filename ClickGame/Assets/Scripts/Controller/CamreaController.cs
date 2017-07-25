1960f5762e0dc24574e2373028daa9f59fed7003
SixDayCoder <sixdaycoder@gmail.com>
2017-07-25 16:47:23 +0800

    Add ClickGame, Firest Phase, Infinite Scrolling Background

---
+++ ClickGame/Assets/Scripts/Controller/CamreaController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamreaController : MonoBehaviour {
+
+    // Use this for initialization
+
+    private GameObject player;//玩家
+    private Vector3 offset;//摄像机和玩家的偏移量
+
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        offset = transform.position - player.transform.position; 
+	}
+
+
+    private void AdjustPosition() {
+        transform.position = new Vector3(player.transform.position.x + offset.x, transform.position.y, transform.position.z);
+    }
+
+	// Update is called once per frame
+	void Update () {
+        AdjustPosition();
+        
+	}
+}
---
+++ ClickGame/Assets/Scripts/Controller/PlayController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayController : MonoBehaviour {
+
+    private float moveSpeed;
+
+
+	// Use this for initialization
+	void Start () {
+        moveSpeed = 2.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.right * Time.deltaTime * moveSpeed);
+	}
+}
