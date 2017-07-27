6674abba9c00b9e4a53389233733db7f278fa3dc
SixDayCoder <sixdaycoder@gmail.com>
2017-07-27 18:00:20 +0800

    Add project AssteBundle and test it

---
+++ AssetBundle/Assets/Scripts/AssetBundle/LoadFromFile.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LoadFromFile : MonoBehaviour {
+
+
+    private string sharePath = "AssetBundles/share.unity3d";
+    private string cubePath = "AssetBundles/prefab/cubewall.unity3d";
+    // Use this for initialization
+    void Start () {
+        //AssetLoadFromFile(); 
+        //StartCoroutine(AssetLoadFromMemoryAsync()); 
+        //AssetLoadFromMemory();
+        //StartCoroutine(LoadFromCacheOrDownload());
+        StartCoroutine(WebRequest());
+	}
+
+    void AssetLoadFromFile() {//也有异步的方法
+        //相对路径
+        AssetBundle share = AssetBundle.LoadFromFile(sharePath);
+        //加载是被加载到内存当中
+        AssetBundle ab = AssetBundle.LoadFromFile(cubePath);
+        //加载assetbundle,如果没有加载它的依赖是不能正确显示cubeWall的, 不存在加载的先后顺序,在LoadAsset之前即可
+        GameObject cubeWall = ab.LoadAsset<GameObject>("CubeWall");//获取prefab
+        Instantiate(cubeWall, Vector3.zero, Quaternion.identity);//实例化prefab
+    }
+
+    void AssetLoadFromMemory() {
+        AssetBundle share = AssetBundle.LoadFromMemory(File.ReadAllBytes(sharePath));
+        AssetBundle ab = AssetBundle.LoadFromMemory(File.ReadAllBytes(cubePath));
+        GameObject cubeWall = ab.LoadAsset<GameObject>("CubeWall");//获取prefab
+        Instantiate(cubeWall, Vector3.zero, Quaternion.identity);//实例化prefab
+    }
+
+    IEnumerator AssetLoadFromMemoryAsync() {
+        //从内存中异步加载,如果服务器使用的tcp协议传输的是byte数组到本地,那么可以使用这个方法读取AssetBundle到内存
+        AssetBundleCreateRequest request = AssetBundle.LoadFromMemoryAsync(File.ReadAllBytes(sharePath));
+        yield return request;//等待加载完share
+
+
+        request = AssetBundle.LoadFromMemoryAsync(File.ReadAllBytes(cubePath));
+        yield return request;//等待加载完request
+
+        //加载完毕,可以使用
+        AssetBundle ab = request.assetBundle;
+        GameObject cubeWall = ab.LoadAsset<GameObject>("CubeWall");//获取prefab
+        Instantiate(cubeWall, Vector3.zero, Quaternion.identity);//实例化prefab
+    }
+
+    //WWW,被UnityWenRequest所代替
+    IEnumerator LoadFromCacheOrDownload() {
+        while (!Caching.ready)
+            yield return null;
+
+        //若第一次下载,下载到cache中,之后从cache中取,注意使用www的时候要完整路径
+        WWW www = WWW.LoadFromCacheOrDownload(@"file://E:\UnityDemos\AssetBundle\AssetBundles\share.unity3d", 1);
+        yield return www;
+
+        if (string.IsNullOrEmpty(www.error)) {
+            www = WWW.LoadFromCacheOrDownload(@"file://E:\UnityDemos\AssetBundle\AssetBundles\prefab\cubewall.unity3d", 1);
+            yield return www;
+
+            //加载完毕,可以使用,如果是远程加载,换成服务器的url就可以
+            AssetBundle ab = www.assetBundle;
+            GameObject cubeWall = ab.LoadAsset<GameObject>("CubeWall");//获取prefab
+            Instantiate(cubeWall, Vector3.zero, Quaternion.identity);//实例化prefab
+
+        }
+        else {
+            Debug.Log(www.error);
+            yield break;//相当于void的return
+        }
+
+    }
+
+    //Unity5.3及以上推荐的方式
+    IEnumerator WebRequest() {
+        string shareURL = @"http://localhost/AssetBundles/share.unity3d";
+        string cubeURL = @"http://localhost/AssetBundles/prefab/cubewall.unity3d";
+
+        UnityWebRequest request = UnityWebRequest.GetAssetBundle(shareURL);
+        yield return request.Send();//发送http请求
+        AssetBundle share = (request.downloadHandler as DownloadHandlerAssetBundle).assetBundle;
+
+        request = UnityWebRequest.GetAssetBundle(cubeURL);
+        yield return request.Send();//发送http请求
+        AssetBundle ab = (request.downloadHandler as DownloadHandlerAssetBundle).assetBundle;
+
+        GameObject cubeWall = ab.LoadAsset<GameObject>("CubeWall");//获取prefab
+        Instantiate(cubeWall, Vector3.zero, Quaternion.identity);//实例化prefab
+    }
+	
+}
---
+++ AssetBundle/Assets/Scripts/Editor/BuildAssetBundle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using System.IO;
+
+
+public class BuildAssetBundle{
+
+    [MenuItem("AssetBundle/Build All AssetBundle")]
+    static void BuildAllAssetBundle() {
+        //BuildPipeline.BuildAssetBundles();
+        string path = "AssetBundles";//相对目录,和Asset同级
+        if (!Directory.Exists(path)) {
+            Directory.CreateDirectory(path);
+        }
+
+        BuildPipeline.BuildAssetBundles(path, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+        //None使用LZMA算法压缩,被压缩的包相比LZ4更多,但是解压时间更久,加载时间更久，解压是必须整体解压
+        //ChunkBasedCompressor LZ4压缩 可以指定加载具体的资源而无需全部解压
+    }
+    /*
+     * CubeWall和SphereWall都引用了同一套贴图和材质,直接打包那么会重复
+     * 最好是依赖打包，把公用的贴图和材质打包,这样会减少内存 
+     * 依赖打包实际上是手动多次分配assetbundle的name多次打包 依赖关系会由unity自行设定
+     */
+
+
+    /*
+     * 1.经常更新的资源单独打包，和不经常更新的资源分开
+     * 2.被公共引用的资源单独打包
+     */
+
+}
