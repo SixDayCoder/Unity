cd2b736bac175481ffb6eb54e52011dc9b7a144f
SixDayCoder <sixdaycoder@gmail.com>
2017-08-01 17:41:08 +0800

    Add GridManager and Knapsack Manager

---
+++ Knapsack/Assets/Scripts/Knapsack/Manager/KnapsackManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnapsackManager {
+
+    #region singleton
+    private KnapsackManager() {}
+    private KnapsackManager _instance = null;
+    public KnapsackManager Instance {
+        get {
+            if (_instance == null)
+                _instance = new KnapsackManager();
+            return _instance;
+        }
+    }
+    #endregion
+
+    private GameObject FindFirstEmptyGrid() {
+        return new GameObject();
+    }
+
+    public void AddItem() {
+        
+    }
+
+    public void RemoveItem() {
+
+    }
+
+    public void LoadItems() {//从数据库中获取json文件并解析
+
+    }
+
+}
