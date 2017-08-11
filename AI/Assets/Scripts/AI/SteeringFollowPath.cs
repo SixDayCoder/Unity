71d7fa01153fb1d6f8bf10d4891fb0e84dd6a545
SixDayCoder <sixdaycoder@gmail.com>
2017-08-11 14:02:33 +0800

    添加随机徘徊、寻路行为

---
+++ AI/Assets/Scripts/AI/SteeringFollowPath.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteeringFollowPath : Steering {
+
+    //路径点标识
+    public GameObject[] wayPoints = new GameObject[5];
+
+    //AI
+    private AILocomotion locomotion;
+
+    //当前节点的序号
+    private int currentNode = 0;
+
+    //当前的target
+    private Transform currentTarget;
+
+    //当和路径点的距离小于该值时,认为已经到达,开始寻找一下点
+    private float arriveDistance;
+
+    //当和目标距离小于该值时,开始减速 
+    private float slowDownDistance;
+
+    private void Start() {
+
+        locomotion = GetComponent<AILocomotion>();
+
+        arriveDistance = 1.0f;
+
+        slowDownDistance = 1.0f;
+
+        currentNode = 0;
+
+        currentTarget = wayPoints[currentNode].GetComponent<Transform>();
+    }
+
+    public override Vector3 Force() {
+
+        Vector3 toTarget = currentTarget.position - transform.position;
+
+        if (locomotion.isPlanar) {
+
+            toTarget.y = 0;
+
+        }
+
+        float distance = toTarget.magnitude;
+
+        //如果是路径点的最后一个节点
+        if(currentNode == wayPoints.Length - 1) {
+
+            //如果到了减速的距离,减速
+            if(distance < slowDownDistance) {
+                //预期速度
+                Vector3 desiredVelocity = toTarget - locomotion.velocity;
+                //操控力
+                return desiredVelocity - locomotion.velocity;
+            }
+            //否则,seek行为
+            else {
+                //预期速度
+                Vector3 desiredVelocity = toTarget.normalized * locomotion.maxSpeed;
+                //操控力
+                return desiredVelocity - locomotion.velocity;
+            }
+        }
+        else {
+            //如果到达了当前路径点,那么准备变换到下一个路径点 
+            if(distance < arriveDistance) {
+
+                //切换target目标
+                currentNode++;
+
+                currentTarget = wayPoints[currentNode].transform;
+
+            }
+
+            //计算预期速度和操控力
+            Vector3 desiredVelocity = toTarget.normalized * locomotion.maxSpeed;
+
+            return desiredVelocity - locomotion.velocity;
+
+        }
+  
+    }
+
+}
---
+++ AI/Assets/Scripts/AI/SteeringForWander.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteeringForWander : Steering {
+
+    //AI
+    private AILocomotion locomotion;
+
+    //徘徊圆圈的半径
+    private float wanderRadius;
+
+    //徘徊距离,物体和徘徊圆圈中点的距离
+    private float wanderDistance;
+
+    //每帧随机小位移的值
+    private float wanderJitter;
+
+    //在wander圈上虚拟出的物体的位置
+    private Vector3 circleTarget;
+
+    private void Start() {
+
+        locomotion = GetComponent<AILocomotion>();
+
+        //徘徊距离越大,物体旋转的角度越小
+        wanderDistance = 2.0f;
+
+        wanderRadius = 1.0f;
+
+        wanderJitter = 1.0f;
+
+        //给定一个圆上位置的初始值,这里我把它放到了距离AI物体最远的圆上的点
+        circleTarget = transform.forward.normalized * (wanderDistance + wanderRadius);
+    }
+
+    public override Vector3 Force() {
+
+        //计算随机位移
+        Vector3 displacement = new Vector3(Random.value * wanderJitter, Random.value * wanderJitter, Random.value * wanderJitter);
+
+        if (locomotion.isPlanar) {
+
+            displacement.y = 0;
+
+        }
+
+        //1.让wander圈上的点随机位移
+        circleTarget += displacement;
+
+        //2.由于移动后可能会超出wander圈,把新的位置投影到圈上 ? 为什么这样可以
+        circleTarget = circleTarget.normalized * wanderRadius;
+
+        //3.现在计算的值是相对于AI角色和AI角色的前进方向的，需要转换为世界坐标 
+        Vector3 targetWorld = locomotion.GetComponent<Transform>().TransformPoint(circleTarget);
+
+        //计算预期速度
+        Vector3 desiredVelocity = (targetWorld - transform.position).normalized * locomotion.maxSpeed;
+
+        return (desiredVelocity - locomotion.velocity);
+
+    }
+
+    private void OnDrawGizmos() {
+
+        Gizmos.DrawWireSphere(transform.forward.normalized * wanderDistance, wanderRadius);
+
+    }
+}
